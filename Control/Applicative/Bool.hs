eb3ef98587321996fe4b941467c8e3e05430dc64
Craig Roche <cdxr01@gmail.com>
2013-12-07 14:11:36 -0500

    initial repository

---
+++ Control/Applicative/Bool.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Control.Applicative.Bool where
+
+import Control.Applicative
+import Data.Monoid
+
+
+class AppBool b where
+    (<&&>) :: b -> b -> b
+    (<||>) :: b -> b -> b
+    true  :: b
+    false :: b
+
+infixl 3 <&&>
+infixl 2 <||>
+
+
+sequenceAnd :: (AppBool b) => [b] -> b
+sequenceAnd = foldr (<&&>) true
+
+sequenceOr :: (AppBool b) => [b] -> b
+sequenceOr = foldr (<||>) false
+
+
+instance AppBool Bool where
+    (<&&>) = (&&)
+    (<||>) = (||)
+    true  = True
+    false = False
+
+instance AppBool Any where
+    Any a <&&> Any b = Any $ a && b
+    Any a <||> Any b = Any $ a || b
+    true  = Any True
+    false = Any False
+
+instance AppBool All where
+    All a <&&> All b = All $ a && b
+    All a <||> All b = All $ a || b
+    true  = All True
+    false = All False
+
+instance (Applicative f, AppBool b) => AppBool (f b) where
+    a <&&> b = (<&&>) <$> a <*> b
+    a <||> b = (<||>) <$> a <*> b
+    true = pure true
+    false = pure false
